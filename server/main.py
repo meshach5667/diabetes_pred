e9af704c6a21847c9d3a12472bc5a0185a23d4bb
meshach5667 <meshachzakumi@gmail.com>
2026-01-13 05:59:59 +0100

    cors

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:8501",
         "http://127.0.0.1:8080",
-        "http://127.0.0.1:5173",
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
